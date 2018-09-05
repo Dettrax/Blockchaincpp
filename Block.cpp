56fbcdcd4363ce619eb3539770f4ed6886f4cab7
Ashish Pandey <39861850+Dettrax@users.noreply.github.com>
2018-09-05 18:17:20 +0530

    Update Block.cpp

--- Block.cpp
+++ Block.cpp
@@ -38,133 +38,107 @@ void Block::MineBlock(uint32_t nDifficulty)
             exit(4);
         }
     }
-    int line_count=0,check_count;
-    fp.open("database.txt",ios::in);
-    while(getline(fp,input))
-        line_count++;
-    check_count = line_count-1;
-
-
-    fp.close();
-
-    //CHEECK
-    fp.open("database.txt",ios::in);
-    char temp_prevstr[64],temp_currstr;
-    while(getline(fp,input))
+  fp.close();
+   // PREV check
+    ifstream fp3;
+     char temp_hashcheck[64];
+     fp3.open("database.txt",ios::in);
+      getline(fp3,input);
+     int j,i;
+     for(j=0,i=3+64;i<3+64+64;i++,j++)
+       temp_hashcheck[j]=input[i];
+    temp_hashcheck[j]='\0';
+    fp3.close();
+    string str_hashcheck(temp_hashcheck);
+    fp3.open("database.txt",ios::in);
+    getline(fp3,input);
+    cout<<"\n";
+    while(getline(fp3,input))
     {
-    if(check_count==line_count)
-        break;
+       /* cout<<"\n";
+        for(j=0;j<64;j++)
+            cout<<temp_hashcheck[j];
+        cout<<"\n";
+        for(j=0,i=3;i<3+64;i++,j++)
+            cout<<input[i];*/
+        for(j=0,i=3;i<3+64;i++,j++)
+      {
+          if(input[i] != temp_hashcheck[j])
+        {
 
-    check_count++;
-    int i,j;
-    for(i=3+65,j=0;i<3+65+64;i++,j++)
-        temp_prevstr[j]=input[i];
-
-    temp_prevstr[j]='\0';
-    getline(fp,input);
-    {
-       int flag=0;
-        if(line_count>1)
-        for(i=3,j=0;i<4+63;i++,j++)
-          {
-              if(input[i] != temp_prevstr[j])
-             flag =1;
-          }
-         if(flag==1)
-            {
-                cout<<"\nInvalid by block \n"<<endl;
-                exit(1);
-            }
-
+            cout<<"invallid by prev "<<endl;
+            exit(1);
+        }
+      }
+        for(j=0,i=3+64;i<4+64+64;j++,i++)
+            temp_hashcheck[j] = input[i];
     }
-    fp.seekg(-52, ios::cur);
-    }
-    fp.close();
+    fp3.close();
 
+ // hash validity
 
+// ifstream fp;
+ fp.open("database.txt",ios::in);
+ int line_count =0;
+ while(getline(fp,input))
+    line_count++;
+ fp.close();
+char ch_index[2], ch_previoushash[64],ch_currenthash[64],ch_time[10],ch_nonce[10],ch_data[40];
+fp.open("database.txt",ios::in);
 
-int temp_index=0;
+while(getline(fp,input))
 {
-   // Chain verify
-   ifstream fp3;
-
-   fp3.open("database.txt",ios::in);
-   while(getline(fp3,input))
-   {
-       temp_index++;
-   }
-   if(_nIndex<=temp_index)
-   {
-       int i,j;
-    fp.open("database.txt",ios::in);
-     int line_count=0,check_count=0;
-    while(getline(fp,input))
-        line_count++;
-    fp.close();
-    fp.open("database.txt",ios::in);
-    int flag =1;
-    while(check_count<line_count && flag==1)
-    {
-        check_count++;
-        getline(fp,input);
-    for(i=0;input[i]!=' ';i++)
-        chIndex[i] = input[i];
-    chIndex[i]='\0';
-    str_chIndex.assign(chIndex);
-    i = i+2;
-    for(j=0;j<64;j++,i++)
-        chPrevHash[j]=input[i];
-    chPrevHash[j] = '\0';
-   str_chprevhash.assign(chPrevHash);
-
-
-    for(j=0;j<63;j++,i++)
-        chcurrhash[j]=input[i];
-    chcurrhash[j]='\0';
-    str_chcurrhash.assign(chcurrhash);
-    i++;
-    for(j=0;j<10;i++,j++)
-     chTime[j]=input[i];
-    chTime[j]='\0';
-    temp_chTime.assign(chTime);
-
-
-
+    ch_index[0] = input[0];
+    ch_index[1] = '\0';
+    str_chIndex.assign(ch_index);
+    for(i=3,j=0;i<3+64;i++,j++)
+        ch_previoushash[j]=input[i];
+    ch_previoushash[j] = '\0';
+    str_chprevhash.assign(ch_previoushash);
+    for(i=4+64,j=0;i<=3+64+64;i++,j++)
+          ch_currenthash[j]=input[i];
+    ch_currenthash[j] = '\0';
+    str_chcurrhash.assign(ch_currenthash);
+    i--;
+    for(j=0;j<10;j++,i++)
+        ch_time[j] = input[i];
+    ch_time[j] = '\0';
+    temp_chTime.assign(ch_time);
     for(j=0;input[i]!=' ';i++,j++)
-        chNonce[j]=input[i];
-    chNonce[j]='\0';
-    i++;
-    for(j=0;input[i]!='\0';j++,i++)
-       chData[j]=input[i];
-    temp_chNonce.assign(chNonce);
-    temp_chData.assign(chData);
+        ch_nonce[j] = input[i];
+    ch_nonce[j] = '\0';
+    temp_chNonce.assign(ch_nonce);
 
-    string verify = _CheckCalculateHash();
-    char c_str[nDifficulty + 1];
+    for(j=0,i++;input[i]!='\0';i++,j++)
+        ch_data[j] = input[i];
+    ch_data[j]  = '\0';
+    temp_chData.assign(ch_data);
+
+
+  /*cout<<"\n"<<input<<endl;
+   cout<<str_chIndex<<str_chprevhash<<temp_chTime<<temp_chNonce<<temp_chData<<endl;
+    */cout<<endl;
+    string check_block = _CheckCalculateHash();
+    char cstr[nDifficulty + 1];
     for (uint32_t i = 0; i < nDifficulty; ++i)
     {
-        c_str[i] = '0';
+        cstr[i] = '0';
     }
-    c_str[nDifficulty] = '\0';
-
-    string check_str(c_str);
-    cout<<verify<<endl;
-    for(int k=0;k<4;k++)
-        if(verify[k]!='0')
-         flag=0;
+    cstr[nDifficulty] = '\0';
+    string str(cstr);
+    for(int k=0;k<nDifficulty;k++)
+    {
+        if(check_block[k] != cstr[k])
+        {
+            cout<<"Invalid by hash "<<endl;
+            exit(2);
+        }
     }
 
 
-if(flag==0)
-{
-    cout<<temp_index<<endl;
-    cout<<"Invalid by hash ";
-    exit(2);
+
 }
-   }
-
 fp.close();
-}
 
 
 
@@ -190,6 +164,8 @@ fp.close();
      fp1.open("database.txt",ios::out);
      fp1<<_nIndex<<" "<<nDifficulty<<sPrevHash<<sHash<< _tTime<< _nNonce<<" "<< _sData<<"\n";
      fp1.close();
+     //cout<<sPrevHash<<endl;
+     //cout<<sHash<<endl;
      }
      else if(_nIndex>1&&file_check==0)
      {
@@ -220,12 +196,13 @@ fp.close();
      {
         ofstream fp2;
         ifstream fp1;
-     fp1.open("Database.dat",ios::in);
+     fp1.open("database.txt",ios::in);
      fp2.open("store.txt",ios::out);
       while(getline(fp1,input))
          {
              int y;
              char store_flag[50];
+             //cout<<"Block "<<input[0]<<" Data is :";
              int k;
              for(k=0;input[k]!='_';k++);
              for(y=0;input[k]!='\0';k++,y++)
@@ -241,6 +218,7 @@ fp.close();
      }
 
     cout << "\nBlock mined: " << sHash << endl;
+    //cout<<_nIndex<<" " <<nDifficulty<<" "<< sPrevHash<<" " <<sHash<<" "<< _tTime<<" " << _nNonce<<" " << _sData<<"\n";
 
 }
 
@@ -254,11 +232,15 @@ inline string Block::_CheckCalculateHash() const
 }
 inline string Block::_CalculateHash() const
 {
+    //stringstream ss;
     string timep,indexp,noncep,res;
     timep = to_string(_tTime);
     indexp = to_string(_nIndex);
     noncep = to_string(_nNonce);
+    //ss << _nIndex << sPrevHash << timep << _sData << _nNonce;
     res = indexp + sPrevHash + timep + _sData + noncep;
+   // cout<<res<<"\n\n";
+    //cout<<res.length()<<endl;
 
 
             return sha256(res);
