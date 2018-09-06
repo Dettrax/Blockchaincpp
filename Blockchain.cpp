2a956a585478ac089c7b3630e7228373aed09d4a
Ashish Pandey <39861850+Dettrax@users.noreply.github.com>
2018-09-06 19:27:17 +0530

    Update Blockchain.cpp

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -1,21 +1,13 @@
-
 #include<fstream>
 #include "Blockchain.h"
-
 Blockchain::Blockchain()
 {
     _vChain.emplace_back(Block(0, "Genesis Block",0,0));
     _nDifficulty = 4;
 }
-
 void Blockchain::AddBlock(Block bNew)
 {
-
-
     ifstream fp;
-
-
-
     fp.open("database.txt",ios::in);
     string input;
     char temp_str[64];
@@ -27,7 +19,6 @@ void Blockchain::AddBlock(Block bNew)
     temp_str[j]='\0';
     }
     string str(temp_str);
-   // cout<<str;
     string  st = _GetLastBlock().sHash;
     if(bNew._nIndex==1)
     bNew.sPrevHash = st;
@@ -36,7 +27,6 @@ void Blockchain::AddBlock(Block bNew)
     bNew.MineBlock(_nDifficulty);
     _vChain.push_back(bNew);
 }
-
 Block Blockchain::_GetLastBlock() const
 {
     return _vChain.back();
